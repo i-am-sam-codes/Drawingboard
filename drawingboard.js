d81380ff0d3e707001f885d77d0b03c60f57a814
Samanta Ceja <sammmceja@gmail.com>
2021-11-01 17:25:33 -0700

    Working on color changing funtionallity

--- drawingboard.js
+++ drawingboard.js
@@ -1,31 +1,58 @@
-const container = document.querySelector('.wrapper');
+//here we select the div from our HTML file
+const container = document.querySelector('.container');
 
 
 
+//creating our grid takes in a size and creates SIZE amount of divs in the container class we called 
 function createGrid(size) {
     let gridSize = size * size;
-    let grid = document.getElementById('cont');
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    
     for (let i = 0; i <= gridSize; i++) {
+        //creates divs
         const div = document.createElement('div');
+        //adds them to css class called 'cell'
         div.classList.add('cell');
-        //div.style.gridTemplateColumns = `${size}, 1fr`;
-        //div.style.gridTemplateRows = `${size}, 1fr`;
-       
-        
-        document.body.appendChild(div);
+        //add an eventListenter that listens for a mouse enter on event it changes the background color to black 
+        div.addEventListener('mouseenter', function( event ) {
+            event.target.style.backgroundColor = 'black';
+            
+            
+        })
+        container.appendChild(div);
     }
+    
+
+};
+
+createGrid(16);
+
+//function to remove all child nodes from parent 
 
 
-}
+
+/*function changeBlue() {
+    const blueButton = document.createElement('button');
+
+    blueButton.classList.add('blue');
+
+    blueButton.addEventListener('click', function ( event ) {
+        event.target.style.backgroundColor = 'blue';
+    })
+}*/
 
 
 
 
+//create a color switch function : switch statment for color changes 
 function changeColor() {
-    switch(color) {
-        case 'black':
+    document.getElementById('blue').addEventListener('click', function() {
+        
+        document.querySelector('.cell').style.backgroundColor = 'blue';
+    })
+    
+    
+};
 
-    }
-}
