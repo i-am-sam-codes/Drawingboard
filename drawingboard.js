814c73d89b97de471836b42718fad7a2af3c9632
Samanta Ceja <sammmceja@gmail.com>
2021-11-03 19:50:28 -0700

    Fixed grid size function

--- drawingboard.js
+++ drawingboard.js
@@ -10,7 +10,7 @@ function createGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-    for (let i = 0; i <= gridSize; i++) {
+    for (let i = 0; i < gridSize; i++) {
         //creates divs
         const div = document.createElement('div');
         //adds them to css class called 'cell'
@@ -31,12 +31,27 @@ createGrid(16);
 //function to prompt the user to change grid size
 function changeGridSize() {
     const message = prompt('Enter a number for your grid!');
-    
+    const nodeList = document.querySelectorAll('.cell');
+    nodeList.forEach(element => element.remove())
+    // while (removeElement.length > 0) {
+        // removeElement[0].remove();
+        // removeElement[0].parentNode.removeChild(removeElement[0]);
+    // }
+    // removeElement.container.removeChild(removeElement);
+    // for (let i = 0; i <= removeElement.length ; i--) {
+        // const element = removeElement[i];
+        // element.remove(removeElement[i]);
+    // }
+ 
+    // removeElement.remove();
+
     if (message != null) {
         const size = message;
         createGrid(size);
     };
    
+   
+
 };
 //function for color change runs through HTML onclick
 function changeColor(newColor) {
