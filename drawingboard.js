b2780931f0ebb6f46e433825a68b1b84691da344
Samanta Ceja <sammmceja@gmail.com>
2021-11-07 20:38:38 -0800

    Ensure user input is max of 100

--- drawingboard.js
+++ drawingboard.js
@@ -3,7 +3,6 @@ const container = document.querySelector('.container');
 
 let color = 'black';
 
-
 //creating our grid takes in a size and creates SIZE amount of divs in the container class we called 
 function createGrid(size) {
     const gridSize = size * size;
@@ -21,24 +20,21 @@ function createGrid(size) {
         })
         container.appendChild(div);
     }
-
 };
 
 createGrid(16);
 
-
-
-//function to prompt the user to change grid size and go through node list and remove elements 
+//function to prompt the user to change grid size and go through node list and remove elements
 function changeGridSize() {
-    const message = prompt('Enter a number for your grid!');
-    const nodeList = document.querySelectorAll('.cell');
-    nodeList.forEach(element => element.remove());
-
-    if (message != null) {
+    const message = Number(prompt('Enter a number for your grid!'));
+    if (message > 100) {
+        return alert('Please enter number less than 100!');
+    } else if (message !== null) {
+        const nodeList = document.querySelectorAll('.cell');
+        nodeList.forEach(element => element.remove());
         const size = message;
         createGrid(size);
     };
-   
 };
 //function for color change runs through HTML onclick
 function changeColor(newColor) {
